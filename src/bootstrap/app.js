520a73aff9703fa896da091bc64d534ccd45f9b7
Matheus Alencar <malencar@pm.me>
2019-03-10 21:21:46 -0300

    fix: remove badge evidence view

--- src/bootstrap/app.js
+++ src/bootstrap/app.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import { Helmet } from 'react-helmet'
-import { Route, Switch, BrowserRouter as Router } from 'react-router-dom'
 
-import TTCREvidence from '../iframes/t2cr-evidence'
 import BadgeEvidence from '../iframes/badge-evidence'
 
 import './app.css'
@@ -12,12 +10,7 @@ const App = () => (
     <Helmet>
       <title>Tokens on Trial</title>
     </Helmet>
-    <Router>
-      <Switch>
-        <Route component={TTCREvidence} exact path="/t2cr" />
-        <Route component={BadgeEvidence} exact path="/badge" />
-      </Switch>
-    </Router>
+    <BadgeEvidence />
   </>
 )
 
