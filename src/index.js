eb690eab60a016b90efd0722a49f169588beb03e
Matheus Alencar <malencar@pm.me>
2019-03-12 12:21:12 -0300

    fix: remove random key generation

--- src/index.js
+++ src/index.js
@@ -4,12 +4,7 @@ import ReactDOM from 'react-dom'
 import App from './bootstrap/app'
 
 const render = () => {
-  ReactDOM.render(
-    <App
-      key={process.env.NODE_ENV === 'development' ? Math.random() : undefined}
-    />,
-    document.getElementById('root')
-  )
+  ReactDOM.render(<App />, document.getElementById('root'))
 }
 
 render(App)
