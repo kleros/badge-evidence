a0abc9819f5188011ac69cdb95308eb82429283c
Matheus Alencar <malencar@pm.me>
2019-05-09 12:48:18 -0300

    feat: display token and badge information

--- src/bootstrap/dapp-api.js
+++ src/bootstrap/dapp-api.js
@@ -4,17 +4,23 @@ import Web3 from 'web3'
 const env = process.env.NODE_ENV === 'production' ? 'PROD' : 'DEV'
 const ETHEREUM_PROVIDER = process.env[`REACT_APP_${env}_ETHEREUM_PROVIDER`]
 const IPFS_URL = process.env[`REACT_APP_${env}_IPFS_URL`]
-const S3_URL = process.env[`REACT_APP_${env}_S3_URL`]
 const T2CR_URL = process.env[`REACT_APP_${env}_T2CR_URL`]
+const T2CR_ADDRESS = process.env[`REACT_APP_${env}_T2CR_ADDRESS`]
 
 let eth
-if (process.env.NODE_ENV === 'test')
+let web3
+if (process.env.NODE_ENV === 'test') {
   eth = new Eth(require('ganache-cli').provider())
-else if (window.ethereum) eth = new Eth(window.ethereum)
-else if (window.web3 && window.web3.currentProvider)
+  web3 = new Web3(require('ganache-cli').provider())
+} else if (window.ethereum) {
+  eth = new Eth(window.ethereum)
+  web3 = new Web3(window.ethereum)
+} else if (window.web3 && window.web3.currentProvider) {
   eth = new Eth(window.web3.currentProvider)
-else eth = new Eth(new Eth.HttpProvider(ETHEREUM_PROVIDER))
+  web3 = new Web3(window.web3.currentProvider)
+} else {
+  eth = new Eth(new Eth.HttpProvider(ETHEREUM_PROVIDER))
+  web3 = new Web3(ETHEREUM_PROVIDER)
+}
 
-const web3 = new Web3('http://localhost:8545')
-
-export { eth, IPFS_URL, S3_URL, T2CR_URL, web3 }
+export { eth, IPFS_URL, T2CR_URL, web3, T2CR_ADDRESS }
--- src/iframes/badge-evidence/index.js
+++ src/iframes/badge-evidence/index.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react'
 
 import ArbitrableAddressList from '../../assets/contracts/arbitrable-address-list.json'
-import { eth, T2CR_URL, web3 } from '../../bootstrap/dapp-api'
+import ArbitrableTokenList from '../../assets/contracts/arbitrable-token-list.json'
+import {
+  eth,
+  T2CR_URL,
+  web3,
+  T2CR_ADDRESS,
+  IPFS_URL
+} from '../../bootstrap/dapp-api'
+import UnknownToken from '../../assets/unknown.svg'
 
-import './badge-evidence.css'
+import './t2cr-evidence.css'
 
 class BadgeEvidence extends Component {
   state = {
@@ -36,13 +44,36 @@ class BadgeEvidence extends Component {
       .contract(ArbitrableAddressList.abi)
       .at(arbitrableContractAddress)
 
+    // Get badge information from the latest meta evidence
+    const metaEvidence = await getBadgeVariables({
+      arbitrableContractAddress,
+      web3,
+      ArbitrableAddressList
+    })
+
+    // Get the token address from the dispute ID.
     const tokenAddress = (await arbitrableAddressList.arbitratorDisputeIDToAddress(
       arbitratorContractAddress,
       disputeID
     ))[0]
+
+    // Get token information, if ever submitted to the T2CR.
+    const arbitrableTokenList = eth
+      .contract(ArbitrableTokenList.abi)
+      .at(T2CR_ADDRESS)
+    const tokenSubmissions = await getTokenSubmissions({
+      arbitrableTokenList,
+      tokenAddress
+    })
+    let token
+    if (tokenSubmissions.length > 0)
+      token = await arbitrableTokenList.getTokenInfo(tokenSubmissions[0])
+
     this.setState({
       tokenAddress,
-      badgeAddress: arbitrableContractAddress
+      badgeAddress: arbitrableContractAddress,
+      metaEvidence,
+      token
     })
   }
 
@@ -51,42 +82,141 @@ class BadgeEvidence extends Component {
   }
 
   render() {
-    const { tokenAddress, badgeAddress } = this.state
+    const { tokenAddress, badgeAddress, metaEvidence } = this.state
+    let { token } = this.state
     if (!tokenAddress || !badgeAddress) return null
 
+    console.info(metaEvidence)
+
+    if (token) token.symbolMultihash = `${IPFS_URL}${token.symbolMultihash}`
+    else
+      token = {
+        symbolMultihash: UnknownToken,
+        name: 'Unknown Token',
+        ticker: '',
+        addr: tokenAddress
+      }
+
     return (
-      <div className="BadgeEvidence">
+      <div className="TTCREvidence">
         <h4 style={{ margin: '0 0 12px 0' }}>The Token in Question:</h4>
-        <div className="BadgeEvidence-data">
-          <div className="BadgeEvidence-data-card">
-            <div className="BadgeEvidence-container">
-              <p className="BadgeEvidence-container-multiline BadgeEvidence-label">
-                Address
-              </p>
-              <p className="BadgeEvidence-container-multiline BadgeEvidence-value">
-                {web3.utils.toChecksumAddress(tokenAddress)}
-              </p>
-              <a
-                className="BadgeEvidence-link"
-                href={`${T2CR_URL}/badge/${web3.utils.toChecksumAddress(
-                  badgeAddress
-                )}/${web3.utils.toChecksumAddress(tokenAddress)}`}
-                target="_blank"
-                rel="noopener noreferrer"
+        <div className="TTCREvidence-data-card">
+          <img
+            className="TTCREvidence-symbol"
+            src={token.symbolMultihash}
+            alt="token symbol"
+          />
+          <div className="TTCREvidence-data-separator" />
+          <div className="TTCREvidence-container">
+            <p className="TTCREvidence-container-name">
+              <b>{token.name}</b> {token.ticker}
+            </p>
+            <p className="TTCREvidence-container-multiline TTCREvidence-label">
+              Address
+            </p>
+            <p className="TTCREvidence-container-multiline TTCREvidence-value">
+              {web3.utils.toChecksumAddress(token.addr)}
+            </p>
+            <a
+              className="TTCREvidence-link"
+              href={`${T2CR_URL}/badge/${web3.utils.toChecksumAddress(
+                badgeAddress
+              )}/${web3.utils.toChecksumAddress(tokenAddress)}`}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <p
+                className="TTCREvidence-container-multiline"
+                style={{ marginTop: '10px' }}
               >
-                <p
-                  className="BadgeEvidence-container-multiline"
-                  style={{ margin: 0 }}
-                >
-                  View Submission
-                </p>
-              </a>
-            </div>
+                View Submission
+              </p>
+            </a>
+          </div>
+        </div>
+        <p className="TTCREvidence-plus">+</p>
+        <div className="TTCREvidence-data-card">
+          <img
+            className="TTCREvidence-symbol"
+            src={`${IPFS_URL}/${metaEvidence.variables.symbolURI}`}
+            alt="badge symbol"
+          />
+          <div className="TTCREvidence-data-separator" />
+          <div className="TTCREvidence-container">
+            <p className="TTCREvidence-container-multiline TTCREvidence-label">
+              Badge
+            </p>
+            <p className="TTCREvidence-container-multiline TTCREvidence-value">
+              <b>{metaEvidence.variables.title}</b>
+            </p>
+            <br />
+            <p className="TTCREvidence-container-multiline TTCREvidence-value">
+              {metaEvidence.variables.description}
+            </p>
+            <br />
+            <p className="TTCREvidence-container-multiline TTCREvidence-value">
+              {metaEvidence.variables.criteriaDescription}
+            </p>
+            <a
+              className="TTCREvidence-link"
+              href={`${IPFS_URL}/${metaEvidence.fileURI}`}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <p
+                className="TTCREvidence-container-multiline"
+                style={{ marginTop: '10px' }}
+              >
+                View Criteria
+              </p>
+            </a>
           </div>
         </div>
       </div>
     )
   }
+}
+
+const getBadgeVariables = async ({
+  arbitrableContractAddress,
+  web3,
+  ArbitrableAddressList
+}) => {
+  const arbitrableAddressList = web3.eth.Contract(
+    ArbitrableAddressList.abi,
+    arbitrableContractAddress
+  )
+  const metaEvidenceEvents = (await arbitrableAddressList.getPastEvents(
+    'MetaEvidence',
+    { fromBlock: 0 }
+  )).sort((a, b) => a.blockNumber - b.blockNumber)
+
+  const metaEvidencePath = `${IPFS_URL}${
+    metaEvidenceEvents[metaEvidenceEvents.length - 1].returnValues._evidence
+  }`
+
+  return (await fetch(metaEvidencePath)).json()
+}
+
+const getTokenSubmissions = async ({ arbitrableTokenList, tokenAddress }) => {
+  const ZERO_ID =
+    '0x0000000000000000000000000000000000000000000000000000000000000000'
+  return (await arbitrableTokenList.queryTokens(
+    ZERO_ID,
+    10,
+    [
+      false, // Do not include tokens which are not on the TCR.
+      true, // Include registered tokens.
+      true, // Include tokens with pending registration requests.
+      true, // Include tokens with pending clearing requests.
+      true, // Include tokens with challenged registration requests.
+      true, // Include tokens with challenged clearing requests.
+      true, // Include token if caller is the author of a pending request.
+      true // Include token if caller is the challenger of a pending request.
+    ],
+    true, // Return oldest first.
+    tokenAddress
+  )).values.filter(tokenID => tokenID !== ZERO_ID)
 }
 
 export default BadgeEvidence
